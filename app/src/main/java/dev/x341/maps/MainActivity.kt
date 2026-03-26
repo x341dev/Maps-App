2feeb5a1d23f974c95a381493cd5a0dff68e6a9d
x341dev <admin@x341.dev>
2026-03-26 09:23:03 +0100

    feat: implement navigation and screens for map and marker management

--- app/src/main/java/dev/x341/maps/MainActivity.kt
+++ app/src/main/java/dev/x341/maps/MainActivity.kt
@@ -2,40 +2,26 @@ package dev.x341.maps
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
-import com.google.maps.android.compose.rememberMarkerState
-import com.google.maps.android.compose.rememberUpdatedMarkerState
-import dev.x341.maps.screen.MapScreen
+import dev.x341.maps.navigation.AppNavigation
 import dev.x341.maps.ui.theme.MapsTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MapsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MapScreen(modifier = Modifier.padding(innerPadding), onNavigate = {})
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    AppNavigation()
                 }
             }
         }
     }
 }
-
